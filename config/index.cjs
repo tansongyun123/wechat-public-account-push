21a8a684be3a3bea2592fe62674e905232953b88
tansongyun123 <134830212+tansongyun123@users.noreply.github.com>
2023-05-31 12:47:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,27 +13,27 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'b7c0dc33a26438c03061ed9256f70c76',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河北',
+  CITY: '石家庄',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: 'a谭',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6hIW5z-jq6UV2tJcG_qL3hYZs54',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'yuWrEAV0w9LkeQoDrUpX6Eig8x6vcgoop1WHNGrMAeI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-08',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '阿谭', year: '2001', date: '03-08',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '2001', date: '02-14',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
