2cb20dbe6c10744a2ed18a537c04d43e9557cca3
tansongyun123 <134830212+tansongyun123@users.noreply.github.com>
2023-06-02 19:43:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6hIW5z-jq6UV2tJcG_qL3hYZs54',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'cek6CgpKdDl5WWAugJM0XbDQI1vz9RBLtfF3GR2-w50',
+      useTemplateId: 'LIRN1G504IVTXWpXd0A1rgCr7ppihzfcdd6Ge44KzRE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-08',
       festivals: [
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'cek6CgpKdDl5WWAugJM0XbDQI1vz9RBLtfF3GR2-w50',
+  CALLBACK_TEMPLATE_ID: 'LIRN1G504IVTXWpXd0A1rgCr7ppihzfcdd6Ge44KzRE',
 
   CALLBACK_USERS: [
     {
